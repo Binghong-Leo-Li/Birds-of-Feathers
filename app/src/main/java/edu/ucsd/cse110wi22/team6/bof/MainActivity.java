5b757f4ac5e4c58ff6db189576e1811b82e62f4d
Kevin He <32375681+kevin-he-01@users.noreply.github.com>
2022-02-02 15:10:54 -0800

    Dynamic list of BoFs

--- app/src/main/java/edu/ucsd/cse110wi22/team6/bof/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110wi22/team6/bof/MainActivity.java
@@ -1,14 +1,36 @@
 package edu.ucsd.cse110wi22.team6.bof;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    protected RecyclerView bofRecyclerView;
+    protected RecyclerView.LayoutManager personsLayoutManager;
+    protected BoFsViewAdapter personsViewAdapter;
+
+    // Dummy
+    public List<BoF> dummyBofs = Arrays.asList(
+            new BoF("John Appleseed"),
+            new BoF("William Griswold")
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bofRecyclerView = findViewById(R.id.bof_list);
+
+        personsLayoutManager = new LinearLayoutManager(this);
+        bofRecyclerView.setLayoutManager(personsLayoutManager);
+
+        personsViewAdapter = new BoFsViewAdapter(dummyBofs);
+        bofRecyclerView.setAdapter(personsViewAdapter);
     }
 }
