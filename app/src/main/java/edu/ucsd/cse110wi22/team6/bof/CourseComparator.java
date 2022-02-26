62c620ddf692c30cc3e56697fbe1152fe9634953
Binghong-Leo-Li <97146983+Binghong-Leo-Li@users.noreply.github.com>
2022-02-26 15:59:04 -0800

    CourseComparator Commented

--- app/src/main/java/edu/ucsd/cse110wi22/team6/bof/CourseComparator.java
+++ app/src/main/java/edu/ucsd/cse110wi22/team6/bof/CourseComparator.java
@@ -3,18 +3,21 @@ package edu.ucsd.cse110wi22.team6.bof;
 import java.util.Comparator;
 import java.util.List;
 
+// Following SRP to have a course comparator
 public class CourseComparator implements Comparator<Course> {
     private final List<String> quartersOrdered;
     // Starting from WINTER since that's the first quarter of the year!
 
+    // constructor
     public CourseComparator(List<String> quartersOrdered) {
         this.quartersOrdered = quartersOrdered;
     }
 
+    // Comparing two courses are identical
     @Override
     public int compare(Course c1, Course c2) {
-        int result = Integer.compare(c1.getYear(), c2.getYear());
-        if (result == 0) {
+        int result = Integer.compare(c1.getYear(), c2.getYear()); // comparing year
+        if (result == 0) { // if year is same then compare quarter
             return Integer.compare(quartersOrdered.indexOf(c1.getQuarter()),
                     quartersOrdered.indexOf(c2.getQuarter()));
         }
