523e3c2c1ae04d91a2314094400956cdc90ba5fb
Binghong-Leo-Li <97146983+Binghong-Leo-Li@users.noreply.github.com>
2022-02-26 15:47:29 -0800

    InMemoryStorage Commented

--- app/src/main/java/edu/ucsd/cse110wi22/team6/bof/InMemoryStorage.java
+++ app/src/main/java/edu/ucsd/cse110wi22/team6/bof/InMemoryStorage.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+// Class for representing individual object storage
 public class InMemoryStorage implements IUserInfoStorage {
     private boolean initialized;
     private String name;
@@ -12,45 +13,54 @@ public class InMemoryStorage implements IUserInfoStorage {
 
     private static final InMemoryStorage INSTANCE = new InMemoryStorage();
 
+    // getter
     public static InMemoryStorage getInstance() {
         return INSTANCE;
     }
 
+    // setter
     @Override
     public void setInitialized(boolean initialized) {
         this.initialized = initialized;
     }
 
+    // getter
     @Override
     public boolean isInitialized() {
         return initialized;
     }
 
+    // setter
     @Override
     public void setName(String name) {
         this.name = name;
     }
 
+    // getter
     @Override
     public String getName() {
         return name;
     }
 
+    // setter
     @Override
     public void setCourseList(List<Course> courses) {
         courseList = new ArrayList<>(courses);
     }
 
+    // getter
     @Override
     public List<Course> getCourseList() {
         return new ArrayList<>(courseList);
     }
 
+    // setter
     @Override
     public void setPhotoUrl(String url) {
         photoUrl = url;
     }
 
+    // getter
     @Override
     public String getPhotoUrl() {
         return photoUrl;
