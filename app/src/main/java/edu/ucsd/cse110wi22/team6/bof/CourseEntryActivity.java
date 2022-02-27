0ce06c06ea6f3e02f198d17a2b79de91e9ab1755
Binghong-Leo-Li <97146983+Binghong-Leo-Li@users.noreply.github.com>
2022-02-26 18:27:50 -0800

    CourseEntryActivity Commented

--- app/src/main/java/edu/ucsd/cse110wi22/team6/bof/CourseEntryActivity.java
+++ app/src/main/java/edu/ucsd/cse110wi22/team6/bof/CourseEntryActivity.java
@@ -1,5 +1,12 @@
 package edu.ucsd.cse110wi22.team6.bof;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
@@ -7,17 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
 import java.util.ArrayList;
 import java.util.List;
 
+// class handling UI showing added courses and giving option to add another new course
 public class CourseEntryActivity extends AppCompatActivity {
     private static final String TAG = "CourseEntryActivity";
     private RecyclerView courseListRecyclerView;
@@ -26,60 +26,75 @@ public class CourseEntryActivity extends AppCompatActivity {
     private CourseViewAdapter courseViewAdapter; // TODO: Allow deleting courses
     private Course previousEntry;
 
+    // Checking if we have saved data from add new course activity
     ActivityResultLauncher<Intent> courseEntryFormLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
+                // only update if we have received another new course from AddNewCourseActivity
                 if (result.getResultCode() == Activity.RESULT_OK) {
                     Intent data = result.getData();
                     assert data != null;
                     Course newCourse = Utilities.parseCourse(data.getStringExtra("course"));
                     Log.d(TAG, "Adding course " + newCourse);
+
+                    // adding the new course to the view through view adapter
                     courseViewAdapter.addCourse(newCourse);
+
+                    // setting previous entry
                     previousEntry = newCourse;
                 }
             }
     );
 
+    // Returning valid previous entry as string or null if empty
     private String serializePreviousEntry() {
         return previousEntry == null ? "" : previousEntry.toString();
     }
 
+    // initialization of the UI
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Enter past courses");
         setContentView(R.layout.activity_course_entry);
 
-        if (savedInstanceState != null) {
+        if (savedInstanceState != null) { // if there are new values, then update
             courses.addAll(Utilities.parseCourseList(
                     savedInstanceState.getString("courseList")));
             String prevCourseEntry = savedInstanceState.getString("prevCourseEntry");
-            if (!prevCourseEntry.isEmpty())
+            if (!prevCourseEntry.isEmpty()) // setting correct previous entry
                 previousEntry = Utilities.parseCourse(prevCourseEntry);
             Log.d(TAG, "Recovered courses from bundle: " + courses);
             Log.d(TAG, "Recovered previous course entry from bundle: " + previousEntry);
-        }
+        } // else no need to update
 
+        // button to handle when finished with the whole course adding
         Button doneButton = findViewById(R.id.course_entry_done_button);
         doneButton.setOnClickListener(this::onDone);
+
+        // button to handle when want to add one more course and move on to AddNewCourseActivity
         Button addOneClassButton = findViewById(R.id.add_one_class_button);
         addOneClassButton.setOnClickListener(this::onAddCourse);
 
         courseListRecyclerView = findViewById(R.id.added_course_list);
 
         courseLayoutManager = new LinearLayoutManager(this);
+        // setting layout
         courseListRecyclerView.setLayoutManager(courseLayoutManager);
 
         courseViewAdapter = new CourseViewAdapter(courses);
+        // updating the new view
         courseListRecyclerView.setAdapter(courseViewAdapter);
     }
 
+    // button for add course, moving on to AddNewCourseActivity
     private void onAddCourse(View view) {
         Intent intent = new Intent(this, AddNewCourseActivity.class);
         intent.putExtra("preFillCourse", serializePreviousEntry());
         courseEntryFormLauncher.launch(intent);
     }
 
+    // button for done, moving on to MainActivity
     private void onDone(View view) {
         IUserInfoStorage storage = Utilities.getStorageInstance(this);
 
@@ -92,6 +107,7 @@ public class CourseEntryActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    // putting extra information when moving on to next state
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
