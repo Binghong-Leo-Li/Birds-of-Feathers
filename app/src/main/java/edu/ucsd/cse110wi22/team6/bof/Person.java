14644beca9ce3a07f81f9da0fe550917d8f89a4a
Kevin He <32375681+kevin-he-01@users.noreply.github.com>
2022-02-27 17:45:14 -0800

    Add UUID to Person class

--- app/src/main/java/edu/ucsd/cse110wi22/team6/bof/Person.java
+++ app/src/main/java/edu/ucsd/cse110wi22/team6/bof/Person.java
@@ -3,19 +3,33 @@ package edu.ucsd.cse110wi22.team6.bof;
 import androidx.annotation.NonNull;
 
 import java.util.List;
+import java.util.UUID;
 
 // Person Class to represent student objects
 public class Person implements IPerson {
 
+    UUID uuid;
     String name;
     List<Course> courseList;
     String photoURL;
 
-    // Constructor
+    // Constructor left for compatibility
     public Person(String name, List<Course> courseList, String photoURL) {
+        this(UUID.randomUUID(), name, courseList, photoURL);
+    }
+
+    // Constructor
+    public Person(UUID uuid, String name, List<Course> courseList, String photoURL) {
+        this.uuid = uuid;
         this.name = name;
         this.courseList = courseList;
         this.photoURL = photoURL;
+    }
+
+    // UUID getter
+    @Override
+    public UUID getUUID() {
+        return uuid;
     }
 
     // Name getter
@@ -48,12 +62,12 @@ public class Person implements IPerson {
         this.name = name;
     }
 
-    // Conversion to string
     @NonNull
     @Override
     public String toString() {
         return "Person{" +
-                "name='" + name + '\'' +
+                "uuid=" + uuid +
+                ", name='" + name + '\'' +
                 ", courseList=" + courseList +
                 ", photoURL='" + photoURL + '\'' +
                 '}';
