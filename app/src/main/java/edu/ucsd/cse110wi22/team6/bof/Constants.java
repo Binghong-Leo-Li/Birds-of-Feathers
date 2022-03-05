ea362c1cbe0a3e934ad410b54fd4b9f8e5de8a88
Alex <alex@gaofamily.org>
2022-03-05 14:37:20 -0800

    mem_tests

--- app/src/main/java/edu/ucsd/cse110wi22/team6/bof/Constants.java
+++ app/src/main/java/edu/ucsd/cse110wi22/team6/bof/Constants.java
@@ -1,7 +1,7 @@
 package edu.ucsd.cse110wi22.team6.bof;
 
 // Class storing all the constants
-public class Constants {
+public class gConstants {
     // This is the first argument that gets passed to getSharedPreferences()
     public static final String PREFERENCE_STRING = "bof_preferences";
 }
